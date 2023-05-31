7903b99bdb6cb306265106edd2679a13c71323e8
ridhohrnf <ridhohrnf@gmail.com>
2023-05-31 23:44:20 +0700

    portofolio

---
+++ src/Details.js
@@ -0,0 +1,171 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+	logogradient: logogradient,
+	logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+	name: "Muhammad Ridho Heranof",
+	tagline: "I build things for web",
+	img: profile,
+	about: `	As a recent graduate of Computer Engineering from Andalas University, I have completed a 6-month full-stack development program at Coding ID. During the program, I gained expertise in frontend development using ReactJS, as well as programming with JavaScript and .NET Core for web applications. Additionally, I have experience in database management using MySQL. These skills enable me to design and develop comprehensive and effective software solutions. I also demonstrated strong leadership and teamwork abilities through my involvement in various campus organizations. Combining my technical expertise in coding with my leadership and teamwork capabilities, I am well-equipped to contribute to successful software development projects.
+`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+	linkdein: "https://www.linkedin.com/in/ridhoheranof",
+	github: "https://www.github.com/ridhohrnf",
+	instagram: "https://www.instagram.com/ridhoheranof",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+	{
+		Position: "Tracer Study",
+		Company: `Andalas University`,
+		Location: "Padang",
+		Type: "Part Time",
+		Duration: "October 2021 - Maret 2022",
+	},
+	{
+		Position: "Data Management",
+		Company: `PT Telekomunikasi Indonesia`,
+		Location: "Padang",
+		Type: "Internship",
+		Duration: "Januari 2020 - Maret 2020",
+	},
+	{
+		Position: "IT Consultant Internship",
+		Company: `Zephrum Consultant Limited`,
+		Location: "London",
+		Type: "Internship",
+		Duration: "Januari 2022 - Januari 2023",
+	},
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+	{
+		Position: "Fullstack Developer",
+		Company: "Coding ID",
+		Location: "Online",
+		Type: "Bootcamp",
+		Duration: "Jan 2023 - Maret 2023",
+	},
+	{
+		Position: "Bachelor in Computer Engineering",
+		Company: `Andalas University`,
+		Location: "Padang",
+		Type: "Graduate",
+		Duration: "Aug 2017 - Februari 2023",
+	},
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+	html: html,
+	css: css,
+	js: js,
+	react: react,
+	redux: redux,
+	bootstrap: bootstrap,
+	vscode: vscode,
+	postman: postman,
+	npm: npm,
+	git: git,
+	github: github,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+	{
+		title: "Project title 1",
+		image: projectImage1,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: "HTML/CSS, JavaScript",
+		previewLink: "https://google.com",
+		githubLink: "https://github.com",
+	},
+	{
+		title: "Project title 2",
+		image: projectImage2,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: "HTML/CSS, JavaScript",
+		previewLink: "https://google.com",
+		githubLink: "https://github.com",
+	},
+	{
+		title: "Project title 3",
+		image: projectImage3,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: "HTML/CSS, JavaScript",
+		previewLink: "https://google.com",
+		githubLink: "https://github.com",
+	},
+	{
+		title: "Project title 4",
+		image: projectImage4,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: "HTML/CSS, JavaScript",
+		previewLink: "https://google.com",
+		githubLink: "https://github.com",
+	},
+	{
+		title: "Project title 5",
+		image: projectImage5,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: "HTML/CSS, JavaScript",
+		previewLink: "https://google.com",
+		githubLink: "https://github.com",
+	},
+	{
+		title: "Project title 6",
+		image: projectImage6,
+		description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+		techstack: "HTML/CSS, JavaScript",
+		previewLink: "https://google.com",
+		githubLink: "https://github.com",
+	},
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+	email: "ridhohrnf@gmail.com",
+	phone: "+6282110751943",
+};
