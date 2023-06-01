c02754e4dba86848317d2f9b5825e51b0937c35f
ridhohrnf <ridhohrnf@gmail.com>
2023-06-02 06:28:58 +0700

    perubahan

--- src/Details.js
+++ src/Details.js
@@ -19,12 +19,10 @@ import npm from "./assets/techstack/npm.png";
 import postman from "./assets/techstack/postman.png";
 import figma from "./assets/techstack/figma.png";
 // Porject Images
-import projectImage1 from "./assets/projects/project1.jpg";
-import projectImage2 from "./assets/projects/project2.jpg";
-import projectImage3 from "./assets/projects/project3.jpg";
-import projectImage4 from "./assets/projects/project4.jpg";
-import projectImage5 from "./assets/projects/project5.jpg";
-import projectImage6 from "./assets/projects/project6.jpg";
+import projectImage1 from "./assets/projects/project1.png";
+import projectImage2 from "./assets/projects/project2.png";
+import projectImage3 from "./assets/projects/project3.png";
+import projectImage4 from "./assets/projects/project4.png";
 
 // Logos
 export const logos = {
@@ -35,7 +33,7 @@ export const logos = {
 // Enter your Personal Details here
 export const personalDetails = {
 	name: "Muhammad Ridho Heranof",
-	tagline: "I build things for web",
+	tagline: "A passionate Front-end React Developer based in Padang, Indonesia",
 	img: profile,
 	about: `	As a recent graduate of Computer Engineering from Andalas University, I have completed a 6-month full-stack development program at Coding ID. During the program, I gained expertise in frontend development using ReactJS, as well as programming with JavaScript and .NET Core for web applications. Additionally, I have experience in database management using MySQL. These skills enable me to design and develop comprehensive and effective software solutions. I also demonstrated strong leadership and teamwork abilities through my involvement in various campus organizations. Combining my technical expertise in coding with my leadership and teamwork capabilities, I am well-equipped to contribute to successful software development projects.
 `,
@@ -109,58 +107,49 @@ export const techStackDetails = {
 // Enter your Project Details here
 export const projectDetails = [
 	{
-		title: "Project title 1",
+		title: "Counter and Add Features",
 		image: projectImage1,
-		description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
+		description: `This website is specifically designed as a practice platform for developers who want to improve their skills in using the useState hook in ReactJS. By providing a range of interactive exercises and examples, it offers a hands-on learning experience that allows developers to gain a deeper understanding of how to effectively utilize useState in their React applications. With step-by-step guidance and real-time feedback, this website empowers developers to strengthen their grasp of state management and build more dynamic and responsive user interfaces using ReactJS.`,
 		techstack: "HTML/CSS, JavaScript",
-		previewLink: "https://google.com",
-		githubLink: "https://github.com",
+		previewLink: "https://counter-and-add-features.vercel.app/",
+		githubLink: "https://github.com/ridhohrnf/Counter-and-Add-movie",
 	},
 	{
-		title: "Project title 2",
+		title: "Portofolio Website",
 		image: projectImage2,
-		description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-		techstack: "HTML/CSS, JavaScript",
-		previewLink: "https://google.com",
-		githubLink: "https://github.com",
+		description: `This website serves as a practice platform for utilizing the useState hook in ReactJS and showcases my portfolio as a front-end developer, highlighting my skills, projects, and expertise. It provides an interactive environment for developers to gain hands-on experience with useState and enables potential clients and employers to evaluate my capabilities while also acting as a comprehensive digital resume.`,
+		techstack: "HTML/CSS, JavaScript, ReactJS, Tailwind",
+		previewLink: "https://portofolio-ridho.vercel.app/",
+		githubLink: "https://github.com/ridhohrnf/portofolio",
 	},
 	{
-		title: "Project title 3",
+		title: "Feedback UI",
 		image: projectImage3,
-		description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-		techstack: "HTML/CSS, JavaScript",
-		previewLink: "https://google.com",
-		githubLink: "https://github.com",
+		description: `This is a project Reactjs. It allows users to add, update and delete feedback. It uses a mock REST api with json-server.
+
+This project goes over all of the fundamentals of React including...
+
+Components
+JSX
+Props (proptypes, defaultprops, etc)
+State (Component & App Level)
+Styling
+Handling Events
+Lists & Keys
+Forms
+Context API
+HTTP Requests`,
+		techstack: "HTML/CSS, JavaScript, ReactJS",
+		previewLink: "https://feedback-app-beta-khaki.vercel.app/",
+		githubLink: "https://github.com/ridhohrnf/Feedback-app",
 	},
 	{
-		title: "Project title 4",
+		title: "Food Shop",
 		image: projectImage4,
-		description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-		techstack: "HTML/CSS, JavaScript",
-		previewLink: "https://google.com",
-		githubLink: "https://github.com",
-	},
-	{
-		title: "Project title 5",
-		image: projectImage5,
-		description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-		techstack: "HTML/CSS, JavaScript",
-		previewLink: "https://google.com",
-		githubLink: "https://github.com",
-	},
-	{
-		title: "Project title 6",
-		image: projectImage6,
-		description: `This is sample project description random things are here in description This is sample
-project lorem ipsum generator for dummy content`,
-		techstack: "HTML/CSS, JavaScript",
-		previewLink: "https://google.com",
-		githubLink: "https://github.com",
+		description: `I am currently engaged in the development of a Food Shop Web application. This project utilizes React.js and .NetCore as the core technologies, and it leverages the robust redmango API to enhance its features and capabilities. With this application, users will be able to explore a wide range of food options, place orders, and enjoy a seamless and efficient online shopping experience. Using redmango API That have Features: Redux, Authentication, Payment API, Pagination, Search`,
+		techstack: "ReactJS, Javascript, .netCore",
+		previewLink: "https://food-shop-liart.vercel.app/",
+		githubLink: "https://github.com/ridhohrnf/Food-shop",
 	},
 ];
 
